818ae288116f879e7ae8bc3045762cb81ac0aad2
Anna Stefaniv Oickle <anna.stefaniv@wallawalla.edu>
2026-05-27 08:41:41 -0700

    Moced tool icon button

--- lib/presentation/statistics_table/statistics_table.dart
+++ lib/presentation/statistics_table/statistics_table.dart
@@ -7,8 +7,8 @@ import 'package:vsd/domain/models/statistic.dart';
 import 'package:vsd/presentation/_reusable_components/case_insensitive_text_type.dart';
 import 'package:vsd/presentation/_reusable_components/pulldown_button.dart';
 import 'package:vsd/presentation/_reusable_components/search_bar.dart';
+import 'package:vsd/presentation/_reusable_components/tool_icon_button.dart';
 import 'package:vsd/presentation/chart/multi_statistic_line_chart.dart';
-import 'package:vsd/presentation/statistics_table/tool_icon_button.dart';
 
 class StatisticsTable extends StatefulWidget {
   const StatisticsTable({
