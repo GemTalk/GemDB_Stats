8771a37764e6ba480e6dceafae4c7cf15523aa48
Anna Stefaniv Oickle <anna.stefaniv@wallawalla.edu>
2026-09-16 16:52:36 -0700

    feat: default is maximized window

--- app/macos/Runner/MainFlutterWindow.swift
+++ app/macos/Runner/MainFlutterWindow.swift
@@ -4,7 +4,7 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    let windowFrame = (self.screen ?? NSScreen.main)?.visibleFrame ?? self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
