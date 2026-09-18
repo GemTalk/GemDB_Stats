c49f65609f7eac52a2639421456e166b9cbf6fee
Anna Stefaniv Oickle <134335292+anna-st-40@users.noreply.github.com>
2026-09-18 10:43:27 -0700

    refactor: move to GitHub (#1)
    * move checks to GitHub Workflows
    
    * move build to GW

--- .github/scripts/build_linux.sh
+++ .github/scripts/build_linux.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 cd app
 
 if [[ -n "${CI:-}" ]]; then
-  apt-get update
-  apt-get install -y --no-install-recommends \
+  SUDO=""
+  [[ $EUID -ne 0 ]] && SUDO="sudo"
+
+  $SUDO apt-get update
+  $SUDO apt-get install -y --no-install-recommends \
     build-essential clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
   git config --global --add safe.directory "${FLUTTER_HOME:-/sdks/flutter}"
 fi
